c1d61bc88448ce453416f1144c1f87f83de56f70
Anna Kummer <annamkummer@gmail.com>
2022-03-08 20:17:49 -0700

    ...

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,12 @@
-import photo from '../assets/photo.jpeg';
 import '../scss/App.scss';
 import { Routes, Route } from 'react-router-dom'
-import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
-import { GrDocumentPdf } from 'react-icons/gr'
-import Nav from './Nav'
-import Home from './views/Home'
-import Settings from './views/Settings'
-import Story from './views/Story'
-import Portfolio from './views/Portfolio'
-import Connect from './views/Connect'
 
 function App() {
-  console.log("Hello, World! Welcome to my site.")
   return (
     <div className="App">
-      <Nav />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/story" element={<Story />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/connect" element={<Connect />} />
-      </Routes>
           
     </div>
-  );
+  )
 }
 
 export default App;
