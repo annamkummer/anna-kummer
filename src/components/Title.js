e2d9bf54d602c6fad30b0c036ef683e6988aa675
Anna Kummer <annamkummer@gmail.com>
2022-03-15 21:08:44 -0600

    Add icons for professional links in home section

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,22 @@
 import '../scss/Title.scss';
+import { GoMarkGithub } from 'react-icons/go'
+import { BsLinkedin } from 'react-icons/bs'
+import { VscFilePdf } from 'react-icons/vsc'
+import res from '../assets/resume.png'
+
+// import pdf from '../../../src/assets/Anna_Kummer_Resume.pdf'
 
 function Title() {
   return (
     <section className="section" id="title">
       <div className="title">
-        <p>Home</p>
+        <h1 className="home-name">Anna Kummer</h1>
+        <h2 className="home-title">Web Developer</h2>
+        <div className="prof-links">
+          <GoMarkGithub className="prof-link" />
+          <BsLinkedin className="prof-link" />
+          <img className="prof-link" src={res} alt="resume pdf" />
+        </div>
       </div>
     </section>
   )
