67b560a28c3cf86717cbf97b66b8e2042d36188b
Anna Kummer <annamkummer@gmail.com>
2022-03-16 19:32:44 -0600

    Add image credits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,24 @@ import '../scss/Footer.scss';
 function Footer() {
   return (
     <footer className="footer">
-        <p>Footer</p>
+        <h3>Settings: </h3>
+        <p>toggle font</p>
+        <p>toggle color</p>
+        <div className="img-credits">
+          <p className="credit">Keyboard Photo by 
+            <a className="ref" href="https://unsplash.com/@skabrera?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+              Sergi Kabrera
+            </a> on 
+            <a className="ref" href="https://unsplash.com/s/photos/keyboard?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+              Unsplash
+            </a>
+          </p>
+          <p className="credit">Pink Circles 
+            <a className="ref" href='https://www.freepik.com/vectors/background'>
+              Background vector created by Sketchepedia - www.freepik.com
+            </a>
+          </p>
+        </div>
     </footer>
   )
 }
